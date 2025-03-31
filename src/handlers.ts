018695bb018c256b9f3c41f1742bc991d2973e11
Farid <anod100002@gmail.com>
2025-03-31 12:48:46 +0400

    add start & help commands

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,6 +1,20 @@
-import { Context, InputFile } from "grammy";
+import { Context, InputFile, BotError, GrammyError, HttpError } from "grammy";
 import { isValidURL } from "./utils/URLcheck";
 import { uploader } from "./utils/uploadFromURL";
+
+// TODO: add I18N
+export const startHandler = async (ctx: Context) => {
+  const name: string = ctx.from?.first_name || "Аноним";
+
+  await ctx.reply(
+    `*🖐 Привет, ${name}!* \n\n` +
+      "Я бот, который поможет тебе загрузить файл с URL.\n" +
+      "Просто отправь мне \`/upload <URL>\` и я загружу файл с него.",
+    {
+      parse_mode: "Markdown",
+    },
+  );
+};
 
 export const urlUploadHandler = async (ctx: Context) => {
   const url = ctx.match as string;
@@ -17,5 +31,19 @@ export const urlUploadHandler = async (ctx: Context) => {
       ctx.reply("Произошла ошибка при загрузке файла");
       console.error(error);
     }
+  }
+};
+
+export const catchHandler = (q: BotError) => {
+  const { ctx, error } = q;
+
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+
+  if (error instanceof GrammyError) {
+    console.error("Error in request:", error.description);
+  } else if (error instanceof HttpError) {
+    console.error("Could not contact Telegram:", error);
+  } else {
+    console.error("Unknown error:", error);
   }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Bot } from "grammy";
 import { botToken } from "./config";
-import { urlUploadHandler } from "./handlers";
+import { startHandler, urlUploadHandler, catchHandler } from "./handlers";
 
 const bot = new Bot(botToken as string);
 
+bot.command(["start", "help"], startHandler);
 bot.command("upload", urlUploadHandler);
+
+bot.catch(catchHandler);
 
 void (async () => {
   await bot.init();
